e3c3044705bf0c21ff8d3fe6a43fb032467201de
Khanyi05 <khanyisilemndau@gmail.com>
2025-03-25 19:42:11 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,138 @@
-import logo from './logo.svg';
-import './App.css';
+// App.js
+import nameThatIChooseForAnImage from "./image-1.jpg";
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
+    <div className="main-container">
+      <div className = "card1">
+      <img 
+      src={nameThatIChooseForAnImage} 
+      alt="desk" 
+      className="image" 
+    />
+    <div className="content">
+      <p className="date">4 days ago</p>
+      <p className="title">Post One</p>
+      <p className="description">
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Error eaque
+        temporibus atque facilis, commodi voluptas quisquam ab tenetur
+        obcaecati nisi.
+      </p>
+    </div>
+    <div className="footer">
+      <div className="footerItem">
+        <p className="footerValue">
+          4 <sup>m</sup>
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <p className="footerLabel">READ</p>
+      </div>
+      <div className="footerItem">
+        <p className="footerValue">5132</p>
+        <p className="footerLabel">VIEWS</p>
+      </div>
+      <div className="footerItem">
+        <p className="footerValue">4</p>
+        <p className="footerLabel">COMMENTS</p>
+      </div>
+    </div>
+      </div>
+      <div className="card2">
+      <img 
+      src={nameThatIChooseForAnImage} 
+      alt="desk" 
+      className="image" 
+    />
+    <div className="content">
+      <p className="date">4 days ago</p>
+      <p className="title">Post One</p>
+      <p className="description">
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Error eaque
+        temporibus atque facilis, commodi voluptas quisquam ab tenetur
+        obcaecati nisi.
+      </p>
+    </div>
+    <div className="footer">
+      <div className="footerItem">
+        <p className="footerValue">
+          4 <sup>m</sup>
+        </p>
+        <p className="footerLabel">READ</p>
+      </div>
+      <div className="footerItem">
+        <p className="footerValue">5132</p>
+        <p className="footerLabel">VIEWS</p>
+      </div>
+      <div className="footerItem">
+        <p className="footerValue">4</p>
+        <p className="footerLabel">COMMENTS</p>
+      </div>
+    </div>
+      </div>
+      <div className="card3">
+      <img 
+      src={nameThatIChooseForAnImage} 
+      alt="desk" 
+      className="image" 
+    />
+    <div className="content">
+      <p className="date">4 days ago</p>
+      <p className="title">Post One</p>
+      <p className="description">
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Error eaque
+        temporibus atque facilis, commodi voluptas quisquam ab tenetur
+        obcaecati nisi.
+      </p>
+    </div>
+    <div className="footer">
+      <div className="footerItem">
+        <p className="footerValue">
+          4 <sup>m</sup>
+        </p>
+        <p className="footerLabel">READ</p>
+      </div>
+      <div className="footerItem">
+        <p className="footerValue">5132</p>
+        <p className="footerLabel">VIEWS</p>
+      </div>
+      <div className="footerItem">
+        <p className="footerValue">4</p>
+        <p className="footerLabel">COMMENTS</p>
+      </div>
+    </div>
+      </div>
+      <div className="card4">
+      <img 
+      src={nameThatIChooseForAnImage} 
+      alt="desk" 
+      className="image" 
+    />
+    <div className="content">
+      <p className="date">4 days ago</p>
+      <p className="title">Post One</p>
+      <p className="description">
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Error eaque
+        temporibus atque facilis, commodi voluptas quisquam ab tenetur
+        obcaecati nisi.
+      </p>
+    </div>
+    <div className="footer">
+      <div className="footerItem">
+        <p className="footerValue">
+          4 <sup>m</sup>
+        </p>
+        <p className="footerLabel">READ</p>
+      </div>
+      <div className="footerItem">
+        <p className="footerValue">5132</p>
+        <p className="footerLabel">VIEWS</p>
+      </div>
+      <div className="footerItem">
+        <p className="footerValue">4</p>
+        <p className="footerLabel">COMMENTS</p>
+      </div>
+    </div>
+      </div>
     </div>
   );
 }
